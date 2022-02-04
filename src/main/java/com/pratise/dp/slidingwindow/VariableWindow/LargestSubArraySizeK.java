1966ccbff8e0e05aaec90beb98d35ff02b52dab4
Sajal Makharia <sajal.makharia@gmail.com>
2022-02-04 23:55:17 +0530

    structure added and variable code added

---
+++ src/main/java/com/pratise/dp/slidingwindow/VariableWindow/LargestSubArraySizeK.java
@@ -0,0 +1,27 @@
+package com.pratise.dp.slidingwindow.VariableWindow;
+
+public class LargestSubArraySizeK {
+    public static void main(String[] args) {
+        int[] arr = {1,-1,3,5,-3,5};
+        int sum = 10;
+        int i=0,j=0,maxSize=0;
+        int res=0;
+        while(j< arr.length){
+            res += arr[j];
+
+            if(res<sum){
+                j++;
+            }else if(res==sum){
+                maxSize = Integer.max(j-i+1,maxSize);
+            } else {
+                while(sum<res) {
+                    res -= arr[i];
+                    i++;
+                }
+
+                j++;
+            }
+        }
+        System.out.println(maxSize);
+    }
+}
